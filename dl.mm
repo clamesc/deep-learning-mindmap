31d1c5da8548df52fe157e7b18dc0ae486a7d6fd
Claus Meschede <19242282+clamesc@users.noreply.github.com>
2018-01-16 14:56:41 +0100

    Initial commit

---
+++ dl.mm
@@ -0,0 +1,110 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Deep Learning" STYLE_REF="Beschreibung" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1516103575996"><hook NAME="MapStyle" background="#fcfce9" zoom="1.464">
+    <properties fit_to_viewport="false;" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600.0 px" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+<stylenode TEXT="Stichpunkt" COLOR="#000000" STYLE="fork" MAX_WIDTH="300.0 px">
+<font NAME="Liberation Sans" SIZE="8" BOLD="true"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#666666" size="1"><i>&#160;</i></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</stylenode>
+<stylenode TEXT="Beschreibung" COLOR="#666666" STYLE="fork" MAX_WIDTH="300.0 px">
+<font NAME="Liberation Sans" SIZE="8" BOLD="false" ITALIC="true"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#666666" size="1"><i>&#160;</i></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#ff3333" STYLE="fork" MAX_WIDTH="300.0 px">
+<font NAME="Liberation Sans" SIZE="8" BOLD="false" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#ff9999" STYLE="fork" MAX_WIDTH="300.0 px">
+<font NAME="Liberation Sans" SIZE="8" BOLD="false" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#0000ff" STYLE="fork" MAX_WIDTH="300.0 px">
+<font NAME="Liberation Sans" SIZE="8" BOLD="false" ITALIC="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#ccccff" STYLE="fork" MAX_WIDTH="300.0 px">
+<font NAME="Liberation Sans" SIZE="8" BOLD="false" ITALIC="true"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="#666666" size="1"><i>Ian Goodfellow, </i></font>
+    </p>
+    <p>
+      <font color="#666666" size="1"><i>Yoshua Bengio, </i></font>
+    </p>
+    <p>
+      <font color="#666666" size="1"><i>Aaron Courville</i></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</map>
